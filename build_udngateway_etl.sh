a96eaff6619c130e6d86e7da55611efbcd92b3ac
mtmcduffie <mcduffie.michael@gmail.com>
2016-08-08 14:10:32 -0400

    UDNTECH-604 - Updating start script to include fixtures.

--- build_udngateway_etl.sh
+++ build_udngateway_etl.sh
@@ -1,8 +1,16 @@
 #Grab ETL Code
 rm -Rf udngateway
-git clone https://github.com/hms-dbmi/udngateway.git
+git clone -b develop https://github.com/hms-dbmi/udngateway.git
 
 sed -i -e 's/migrations.RunPython(addsimpleid),/# migrations.RunPython(addsimpleid),/g' udngateway/udngateway/application/migrations/0001_squashed_0042_auto_20151016_1206.py
 sed -i -e 's/migrations.RunPython(fillinprev),/# migrations.RunPython(fillinprev),/g' udngateway/udngateway/login/migrations/0005_fillinethnic.py
+
+sed -i -e 's/msg.send()/# msg.send()/g' udngateway/udngateway/login/views.py
+sed -i -e 's/msg.attach_alternative/# msg.attach_alternative/g' udngateway/udngateway/login/views.py
+sed -i -e 's/msg = EmailMultiAlternatives/# msg = EmailMultiAlternatives/g' udngateway/udngateway/login/views.py
+sed -i -e 's/if sendemail:/# if sendemail:/g' udngateway/udngateway/login/views.py
+
+sed -i -e 's/post_save.connect(handle_sample_changed, sender=Sample)/# post_save.connect(handle_sample_changed, sender=Sample)/g' udngateway/udngateway/biorepo/models.py
+
 
 docker build -t dbmi/udngateway-etl-docker ./
--- start_udngateway_etl.sh
+++ start_udngateway_etl.sh
@@ -5,8 +5,24 @@ docker run -d --name udngateway-etl dbmi/udngateway-etl-docker tail -f /dev/null
 
 docker cp local_dev_new.py udngateway-etl:/opt/udngateway/udngateway/udngateway/settings/local_dev_new.py
 
+docker exec udngateway-etl /bin/sh -c 'mkdir -p /opt/udngateway/udngateway/login/fixtures/'
+docker exec udngateway-etl /bin/sh -c 'mkdir -p /opt/udngateway/udngateway/application/fixtures/'
+docker exec udngateway-etl /bin/sh -c 'mkdir -p /opt/udngateway/udngateway/patient/fixtures/'
+docker exec udngateway-etl /bin/sh -c 'mkdir -p /opt/udngateway/udngateway/photos/fixtures/'
+docker exec udngateway-etl /bin/sh -c 'mkdir -p /opt/udngateway/udngateway/survey/fixtures/'
+docker exec udngateway-etl /bin/sh -c 'mkdir -p /opt/udngateway/udngateway/biorepo/fixtures/'
+
+docker cp login.json udngateway-etl:/opt/udngateway/udngateway/login/fixtures/
+docker cp address.json udngateway-etl:/opt/udngateway/udngateway/application/fixtures/
+docker cp application.json udngateway-etl:/opt/udngateway/udngateway/application/fixtures/
+docker cp patient.json udngateway-etl:/opt/udngateway/udngateway/patient/fixtures/
+docker cp photos.json udngateway-etl:/opt/udngateway/udngateway/photos/fixtures/
+docker cp survey.json udngateway-etl:/opt/udngateway/udngateway/survey/fixtures/
+docker cp biorepo.json udngateway-etl:/opt/udngateway/udngateway/biorepo/fixtures/
+docker cp zips.json udngateway-etl:/opt/udngateway/udngateway/login/fixtures/
+docker cp populate.json udngateway-etl:/opt/udngateway/udngateway/login/fixtures/
+
 docker exec udngateway-etl /bin/sh -c 'cd /opt/udngateway/udngateway/; python manage.py migrate --settings udngateway.settings.local_dev_new'
-docker exec udngateway-etl /bin/sh -c 'cd /opt/udngateway/udngateway/; python manage.py loaddata populate --settings udngateway.settings.local_dev_new'
-docker exec udngateway-etl /bin/sh -c 'cd /opt/udngateway/udngateway/; python manage.py loaddata zips --settings udngateway.settings.local_dev_new'
+docker exec udngateway-etl /bin/sh -c 'cd /opt/udngateway/udngateway/; python manage.py loaddata populate zips login.json address.json application.json patient.json photos.json survey.json biorepo.json --settings udngateway.settings.local_dev_new'
 
 #docker exec -i -t udngateway-etl /bin/bash
